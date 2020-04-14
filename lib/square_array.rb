d4557aeabfad64023b84bfdf9b74093e771e0081
Juan Fernandez <jcfernan@live.com>
2020-04-14 00:11:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,12 +5,3 @@ def square_array(array)
   end
 return new_array
 end
-
-
-def square_array(array)
-  new_array=[]
-  array.each do |Integer|
-    new_array << Integer ** 2
-  end
-return new_array
-end
